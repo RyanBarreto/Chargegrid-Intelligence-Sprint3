18410aa1b285372a25826f0ad8bd7e8e4b548d03
RyanBarreto <144958582+RyanBarreto@users.noreply.github.com>
2026-09-16 14:26:33 -0300

    Adicionando o conteúdo de config.h

--- src/config.h
+++ src/config.h
@@ -1,1 +1,12 @@
+#ifndef CONFIG_H
+#define CONFIG_H
 
+// Credenciais da sua rede Wi-Fi que o ESP32 vai se conectar
+#define WIFI_SSID "Nome_Do_Seu_Wifi"
+#define WIFI_PASS "Sua_Senha_Do_Wifi"
+
+#define SUPABASE_URL "https://xxxx.supabase.co"
+// Dados do seu projeto no Supabase (encontrados em Project Settings > API no Supabase)
+#define SUPABASE_KEY "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..." 
+
+#endif
