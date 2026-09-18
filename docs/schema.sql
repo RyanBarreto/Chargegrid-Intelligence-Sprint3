44d183b4bb7815f6dee43bb89cce455eeaea24ed
RyanBarreto <144958582+RyanBarreto@users.noreply.github.com>
2026-09-18 08:09:12 -0300

    Conteúdo do schema.sql

--- docs/schema.sql
+++ docs/schema.sql
@@ -1,1 +1,6 @@
-
+-- Garante que o aplicativo web consiga atualizar o status do pagamento
+CREATE POLICY "Permitir update do App Web" 
+ON public.Estacoes 
+FOR UPDATE 
+USING (true)
+WITH CHECK (true);
